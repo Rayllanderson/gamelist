abe09d5ae94a2ea48266d108cbb9e91086f4ca78
rayllanderson <rayllanderson@gmail.com>
2021-02-16 15:31:24 -0300

    criação do GameService

---
+++ backend/src/test/java/com/rayllanderson/ServiceTests.java
@@ -0,0 +1,61 @@
+package com.rayllanderson;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rayllanderson.entities.Game;
+import com.rayllanderson.entities.enums.Status;
+import com.rayllanderson.services.GameService;
+import com.rayllanderson.services.exceptions.ObjectNotFoundException;
+
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+class ServiceTests {
+
+    @Autowired
+    private GameService service;
+
+    private Long id;
+    private final Long INEXISTENT_ID = 99899898L;
+
+    @BeforeAll
+    void instantiateGames() {
+	id = service.save(new Game(null, "Death Stranding", Status.WISHED)).getId();
+    }
+
+    @Test
+    void findById() {
+	assertEquals("Death Stranding", service.findById(id).getName());
+    }
+
+    @Test
+    void findAnInexistentId() {
+	assertThatThrownBy(() -> {
+	    service.findById(INEXISTENT_ID);
+	}).isInstanceOf(ObjectNotFoundException.class);
+    }
+
+    @Test
+    void deleteAnInexistentId() {
+	assertThatThrownBy(() -> {
+	    service.deleteById(INEXISTENT_ID);
+	}).isInstanceOf(ObjectNotFoundException.class);
+    }
+
+    @Test
+    void deleteById() {
+	service.deleteById(id);
+
+	assertThatThrownBy(() -> {
+	    service.findById(id);
+	}).isInstanceOf(ObjectNotFoundException.class);
+    }
+
+}
