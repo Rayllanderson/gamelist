e5a390c1a28410fd5983f7bdfdd8e886711f6726
rayllanderson <rayllanderson@gmail.com>
2021-02-16 19:22:37 -0300

    Delete mapping em /games

--- backend/src/main/java/com/rayllanderson/controllers/GameController.java
+++ backend/src/main/java/com/rayllanderson/controllers/GameController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,5 +54,10 @@ public class GameController {
 	return ResponseEntity.ok().build();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> save(@PathVariable Long id){
+	service.deleteById(id);
+	return ResponseEntity.noContent().build();
+    }
 
 }
