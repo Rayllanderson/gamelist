ade067b227ebf96295ee7e6beab14a20752e182c
rayllanderson <rayllanderson@gmail.com>
2021-02-16 19:01:25 -0300

    cadastrado game via POST e handling ConstraintViolationException

--- backend/src/main/java/com/rayllanderson/controllers/GameController.java
+++ backend/src/main/java/com/rayllanderson/controllers/GameController.java
@@ -1,13 +1,17 @@
 package com.rayllanderson.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rayllanderson.entities.Game;
 import com.rayllanderson.repositories.GameRepository;
@@ -31,6 +35,13 @@ public class GameController {
     @GetMapping("/{id}")
     public ResponseEntity<Game> findById(@PathVariable Long id){
 	return ResponseEntity.ok(service.findById(id));
+    }
+    
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody Game game){
+	game = service.save(game);
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(game.getId()).toUri();
+	return ResponseEntity.created(uri).build();
     }
 
 }
--- backend/src/main/java/com/rayllanderson/controllers/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/rayllanderson/controllers/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package com.rayllanderson.controllers.exceptions;
 import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,14 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ObjectNotFoundException.class) //identificando pra quando ocorrer essa exceção, fazer o tratamento abaixo
     public ResponseEntity<StandardError> obectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 	HttpStatus status = HttpStatus.NOT_FOUND;
-	StandardError err = new StandardError(Instant.now(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+	StandardError err = new StandardError(Instant.now(), status.value(), "Not Found", e.getMessage(), request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+    }
+    
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> nameValidation(HttpServletRequest request){
+	HttpStatus status = HttpStatus.BAD_REQUEST;
+	StandardError err = new StandardError(Instant.now(), status.value(), "Bad Request", "Um ou mais campos estão vazios" , request.getRequestURI());
 	return ResponseEntity.status(status).body(err);
     }
 }
