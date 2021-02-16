eef6169b6b9a09c172681d84e1547cedfd275dfa
rayllanderson <rayllanderson@gmail.com>
2021-02-16 19:19:13 -0300

    Put mapping em /games

--- backend/src/main/java/com/rayllanderson/controllers/GameController.java
+++ backend/src/main/java/com/rayllanderson/controllers/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +44,14 @@ public class GameController {
 	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(game.getId()).toUri();
 	return ResponseEntity.created(uri).build();
     }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> save(@PathVariable Long id, @RequestBody Game game){
+	Game gameFromDatabase = service.findById(id);
+	service.updateData(game, gameFromDatabase);
+	service.save(gameFromDatabase);
+	return ResponseEntity.ok().build();
+    }
+
 
 }
