b1948db218cd117c7f38402bf7566f214fb2edd0
rayllanderson <rayllanderson@gmail.com>
2021-02-16 12:50:58 -0300

    criando entidades e testes

---
+++ backend/src/main/java/com/rayllanderson/entities/Game.java
@@ -0,0 +1,72 @@
+package com.rayllanderson.entities;
+
+import java.io.Serializable;
+
+import com.rayllanderson.entities.enums.Status;
+
+public class Game implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private String name;
+    private Status status;
+
+    public Game() {
+    }
+
+    public Game(Long id, String name, Status status) {
+	this.id = id;
+	this.name = name;
+	this.status = status;
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public Status getStatus() {
+	return status;
+    }
+
+    public void setStatus(Status status) {
+	this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Game other = (Game) obj;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	return true;
+    }
+}
