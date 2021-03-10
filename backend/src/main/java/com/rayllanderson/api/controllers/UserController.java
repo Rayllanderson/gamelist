f56a3835cdc27d7f68889eefe41867423f00fb7a
rayllanderson <rayllanderson@gmail.com>
2021-03-10 00:59:59 -0300

    delete /users

--- backend/src/main/java/com/rayllanderson/api/controllers/UserController.java
+++ backend/src/main/java/com/rayllanderson/api/controllers/UserController.java
@@ -11,13 +11,7 @@ import com.rayllanderson.model.repositories.UserRepository;
 import com.rayllanderson.model.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -63,6 +57,12 @@ public class UserController {
     @PutMapping("/{id}/update/data")
     public ResponseEntity<Void> updateNameOrUsername(@PathVariable Long id, @RequestBody UserDetailsDTO user) {
         userService.updateUsernameOrEmail(user, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
