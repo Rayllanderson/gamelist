086840e0a9d951cdc2760f06b2ed8c484b545a3d
rayllanderson <rayllanderson@gmail.com>
2021-03-10 00:56:06 -0300

    update password, email & username /users

--- backend/src/main/java/com/rayllanderson/api/controllers/UserController.java
+++ backend/src/main/java/com/rayllanderson/api/controllers/UserController.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.rayllanderson.model.dtos.GameDTO;
-import com.rayllanderson.model.dtos.user.SaveUserDTO;
+import com.rayllanderson.model.dtos.user.UserDTO;
 import com.rayllanderson.model.dtos.user.UserDetailsDTO;
 import com.rayllanderson.model.repositories.UserRepository;
 import com.rayllanderson.model.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rayllanderson.model.entities.User;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -50,15 +48,22 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDetailsDTO> register(@RequestBody SaveUserDTO user) {
+    public ResponseEntity<UserDetailsDTO> register(@RequestBody UserDTO user) {
         UserDetailsDTO dto = userService.register(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<Void> edit(@PathVariable Long id) {
-        return null;
+    @PutMapping("/{id}/update/password")
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UserDTO user) {
+        userService.updatePassword(user, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}/update/data")
+    public ResponseEntity<Void> updateNameOrUsername(@PathVariable Long id, @RequestBody UserDetailsDTO user) {
+        userService.updateUsernameOrEmail(user, id);
+        return ResponseEntity.noContent().build();
     }
 
 
