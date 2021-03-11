04deb0a71b09829c734896b23f345ae6e2272eba
rayllanderson <rayllanderson@gmail.com>
2021-03-11 02:10:43 -0300

    adicionando spring security

---
+++ backend/src/main/java/com/rayllanderson/api/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.rayllanderson.api.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(); // habilita o form login
+        http.httpBasic().and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+}
