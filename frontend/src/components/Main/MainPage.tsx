e86a53ce4f5eee33f65cade90207511daed1ff55
rayllanderson <rayllanderson@gmail.com>
2021-03-18 01:36:49 -0300

    adicionando modal para adicionar/editar game

--- frontend/src/components/Main/MainPage.tsx
+++ frontend/src/components/Main/MainPage.tsx
@@ -1,5 +1,6 @@
 import { DeleteModal } from "../Modal/DeleteGameModal";
 import { Modal } from "../Modal/Modal";
+import { RegisterGameModal } from "../Modal/RegisterGameModal";
 import { Footer } from "./Footer";
 import { Nav } from "./Nav";
 import { Search } from "./Search";
@@ -19,6 +20,11 @@ export function MainPage() {
 
       <Modal id="deleteModal" title="Atenção" successBtnText="Excluir">
         <DeleteModal gameName="Gta v" />
+      </Modal>
+
+
+      <Modal id="gameModal" title="Novo Jogo" successBtnText="Salvar">
+        <RegisterGameModal/>
       </Modal>
     </div>
   );
