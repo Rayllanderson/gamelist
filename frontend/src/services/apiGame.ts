28fae28fd8b837d5ff2b700abc0955cec9f48031
rayllanderson <rayllanderson@gmail.com>
2021-03-22 07:41:17 -0300

    criando classe GameApi para manipular api nos endpoints /games

---
+++ frontend/src/services/apiGame.ts
@@ -0,0 +1,38 @@
+import api from "./api";
+import authHeader from "./auth";
+
+export interface PostGameData {
+  name: string;
+  gameStatus: string;
+}
+
+export default class ApiGame {
+
+  findAll() {
+    return api.get('/games', { headers: authHeader() });
+  }
+
+  post(data: PostGameData) {
+    return api.post('/games/', data, { headers: authHeader() });
+  }
+
+  findById(id: number) {
+    return api.get('/games/' + id, { headers: authHeader() });
+  }
+
+  put(id: number, data: PostGameData) {
+    return api.put('/games/' + id, data, { headers: authHeader() });
+  }
+
+  delete(id: number) {
+    return api.delete('/games/' + id, { headers: authHeader() });
+  }
+
+  findByStatus(url: string) {
+    return api.get('/games/status/' + url, { headers: authHeader() });
+  }
+
+  findByName(query: string) {
+    return api.get('/games/seacrh?q=' + query, { headers: authHeader() });
+  }
+}
