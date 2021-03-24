dbd5e6ce6ac84598a3889922a91c96b323be0ba9
Rayllanderson <rayllanderson@gmail.com>
2021-03-24 03:54:11 -0300

    fix UNSAFE_componentWillMount

--- frontend/src/components/Main/card/index.tsx
+++ frontend/src/components/Main/card/index.tsx
@@ -1,7 +1,6 @@
-import { useContext } from "react";
 import { FiChevronRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { Game, GameContext } from "../../../contexts/GameContext";
+import { Game } from "../../../contexts/GameContext";
 import { CardGame } from "./styles";
 
 interface Props {
@@ -9,22 +8,15 @@ interface Props {
 }
 
 export default function Card({ game }: Props) {
-  const { edit } = useContext(GameContext)
   return (
     <CardGame>
-      <Link
-        to={`/game/${game.id}`}
-      >
-        <img
-          src="game-controler.svg"
-          alt="logo"
-        />
+      <Link to={`/game/${game.id}`}>
+        <img src="game-controler.svg"
+          alt="logo"/>
         <div>
           <strong>{game.name}</strong>
           <p>{game.status}</p>
         </div>
-        
-
         <FiChevronRight size={20} />
       </Link>
     </CardGame>
