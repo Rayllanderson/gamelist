2fe1cf56c7f883a1b69b9fca5a3461a42fe26118
Rayllanderson <rayllanderson@gmail.com>
2021-03-24 06:47:49 -0300

    restrigindo search

--- frontend/src/contexts/SearchContex.tsx
+++ frontend/src/contexts/SearchContex.tsx
@@ -1,33 +1,56 @@
 
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { info } from 'node:console';
+import { createContext, ReactNode, useCallback, useContext, useState } from 'react';
 import ApiGame from '../services/apiGame';
 import { GameContext } from './GameContext';
+import { ToastContext } from './ToastContext';
 
 interface SearchProviderProps {
   children: ReactNode;
 }
 
 interface SearchContextData {
-  handleChange:(e: any) => void,
-  handleSubmit: (e:any) => void,
-  search:string;
+  handleChange: (e: any) => void,
+  handleSubmit: (e: any) => void,
+  search: string;
 }
 
 export const SearchContext = createContext<SearchContextData>({} as SearchContextData);
 
 export function SearchProvider({ children }: SearchProviderProps) {
   const [search, setSearch] = useState('');
-  const {setGames} = useContext(GameContext)
- 
-  function handleChange(e:any){
-    setSearch(e.target.value);
+  const [canSearch, setCanSearch] = useState(false);
+  const { setGames } = useContext(GameContext);
+  const { addToast } = useContext(ToastContext);
+
+  function handleChange(e: any) {
+    const text = e.target.value;
+    setSearch(text);
+    text.length > 0 && setCanSearch(true);
   }
 
-  function handleSubmit(e:any){
-    e.preventDefault();
-    new ApiGame().findByName(search)
-    .then(response => setGames(response.data))
-    .catch(err => console.log(err))
+  const handleEmptyData = useCallback((data: any[], search: string) => {
+    console.log('sio')
+    data.length === 0 &&
+      addToast({
+        type: 'info',
+        title: 'Nenhum resultado',
+        description: 'Não existe nenhum jogo com "' + search + '" na sua lista de jogos.'
+      })
+  }, [addToast])
+
+  function handleSubmit(e: any) {
+    e.preventDefault(); //
+
+    canSearch &&
+      new ApiGame().findByName(search)
+        .then(response => {
+          setGames(response.data);
+          handleEmptyData(response.data, search)
+          setCanSearch(true);
+          (search.length === 0 && canSearch) && setCanSearch(false);
+        })
+        .catch(err => console.log(err))
   }
 
   return (
