72ef0ab334132ce9bcbb883df8321862671330e2
Rayllanderson <rayllanderson@gmail.com>
2021-03-24 06:47:49 -0300

    restrigindo search

--- frontend/src/components/Main/Search.tsx
+++ frontend/src/components/Main/Search.tsx
@@ -7,7 +7,7 @@ export function Search() {
 
   return (
     <div className='input-group inputSearch input-group-lg' >
-      <input type="text" className="rounded-md py-1 px-3 form-control " placeholder="Pesquise um jogo"
+      <input type="search" className="rounded-md py-1 px-3 form-control " placeholder="Pesquise um jogo"
       value={search} onChange={handleChange} />
       <button type="button" className="btn btn-pink btn-lg"
       onClick={handleSubmit}> &nbsp; <i className="fa fa-search"></i> &nbsp;</button>
