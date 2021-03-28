22c1cf517834d481f6f8313b90e24245b0e81d4f
Rayllanderson <rayllanderson@gmail.com>
2021-03-28 07:50:52 -0300

    add env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://rayllanderson-game-list.herokuapp.com/api/v1.0",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
