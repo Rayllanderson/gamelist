b93682c882df6842602b3f47e39bf9c6f5d07910
Rayllanderson <rayllanderson@gmail.com>
2021-03-28 07:07:06 -0300

    finalizando frontend

--- frontend/src/components/Loaders/Index.tsx
+++ frontend/src/components/Loaders/Index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ContentLoader from "react-content-loader"
 import './style.css'
 
@@ -7,7 +8,6 @@ interface LoaderCircleProps {
 }
 export function LoaderCircle({ size }: LoaderCircleProps) {
   const spinnerSize = size === 'sm' ? 'spinner-border-sm' : ''
-
   return (
     <div className="d-flex justify-content-center loader">
       <div className={`spinner-border ${spinnerSize} text-light`} role="status"></div>
@@ -26,24 +26,34 @@ export function LoaderCircleButton({ message }: LoaderCircleProps) {
 
 export const LoaderCard = () => {
   const loaderLenght = [1, 2, 3];
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 250);
+    if (!isVisible) return () => { };
+  }, [isVisible])
+
   return (
     <div>
-      {loaderLenght.map((key) => <Loader key={key} />)}
+      {isVisible &&
+        loaderLenght.map((key) => <Loader key={key} />)
+      }
     </div>
   );
 }
 
-const Loader = () => (
-  <ContentLoader className="loader" style={{ maxHeight: 250 }}
-    speed={2}
-    viewBox={`0 0 1200 230`}
-    height={"100%"} width={"100%"}
-    backgroundColor="#44475a"
-    foregroundColor="#5c6175"
-  >
-    <rect x="0" y="0" rx="5" ry="5" width="100%" height={"89%"} />
-  </ContentLoader >
-)
+const Loader = () => {
+  return (
+    <ContentLoader className="loader" style={{ maxHeight: 250 }}
+      speed={2}
+      viewBox={`0 0 1200 230`}
+      height={"100%"} width={"100%"}
+      backgroundColor="#44475a"
+      foregroundColor="#5c6175"
+    >
+      <rect x="0" y="0" rx="5" ry="5" width="100%" height={"89%"} />
+    </ContentLoader >
+  )
+}
 
 
 export const LoaderGame = () => (
@@ -68,29 +78,27 @@ export const LoaderGame = () => (
   </ContentLoader>
 )
 
-export const LoaderGameMobile = () => {
-  return (
-    <ContentLoader speed={2} className="loader"
-      width={750}
-      height={450}
-      viewBox="0 0 750 450"
-      backgroundColor="#44475a"
-      foregroundColor="#5c6175">
-      <circle cx="45" cy="40" r="34" />
-      <rect x="91" y="28" rx="5" ry="5" width="220" height="26" />
-     
-      <rect x="48" y="96" rx="5" ry="5" width="63" height="14" />
-      <rect x="49" y="120" rx="5" ry="5" width="120" height="38" />
+export const LoaderGameMobile = () => (
+  <ContentLoader speed={2} className="loader"
+    width={750}
+    height={450}
+    viewBox="0 0 750 450"
+    backgroundColor="#44475a"
+    foregroundColor="#5c6175">
+    <circle cx="45" cy="40" r="34" />
+    <rect x="91" y="28" rx="5" ry="5" width="220" height="26" />
 
-      <rect x="48" y="180" rx="5" ry="5" width="63" height="14" />
-      <rect x="49" y="204" rx="5" ry="5" width="120" height="38" />
+    <rect x="48" y="96" rx="5" ry="5" width="63" height="14" />
+    <rect x="49" y="120" rx="5" ry="5" width="120" height="38" />
 
-      <rect x="48" y="264" rx="5" ry="5" width="63" height="14" />
-      <rect x="49" y="288" rx="5" ry="5" width="120" height="38" />
+    <rect x="48" y="180" rx="5" ry="5" width="63" height="14" />
+    <rect x="49" y="204" rx="5" ry="5" width="120" height="38" />
 
-      <rect x="105" y="360" rx="3" ry="3" width="75" height="40" />
-      <rect x="15" y="360" rx="3" ry="3" width="75" height="40" />
+    <rect x="48" y="264" rx="5" ry="5" width="63" height="14" />
+    <rect x="49" y="288" rx="5" ry="5" width="120" height="38" />
 
-    </ContentLoader>
-  )
-}
+    <rect x="105" y="360" rx="3" ry="3" width="75" height="40" />
+    <rect x="15" y="360" rx="3" ry="3" width="75" height="40" />
+
+  </ContentLoader>
+)
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  //http://192.168.0.122:8080/api/v1.0
-  baseURL: "https://rayllanderson-game-list.herokuapp.com/api/v1.0",
+  baseURL: "localhost:8080/api/v1.0",
 });
 
 export default api;
